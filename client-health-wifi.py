39cdd3a47909fde23db724712d248ae24eb0101a
taimoorahmed91 <taimoorahmed91@gmail.com>
2020-09-02 07:23:26 -0700

    FMC API application count

--- client-health-wifi.py
+++ client-health-wifi.py
@@ -23,14 +23,16 @@ headers = {
 
 response = requests.get(url, headers=headers, verify=False).json()
 
-#print(json.dumps(response, indent=2, sort_keys=True))
+print(json.dumps(response, indent=2, sort_keys=True))
 device_details = response['topology']['nodes']
-for device_detail in device_details:
-    if device_detail['id'] == device_details[0]['id']:
-        print(f"Client IP: {device_detail['ip']}")
-        print(f"MAC: {device_detail['id']}")
-        print(f"Health Score: {device_detail['healthScore']}")
-        print(" ")
-    elif device_detail['id'] == device_details[1]['id']:
-        print(f"Connected to {device_detail['deviceType']}")
-        print(f"WAP IP: {device_detail['ip']}")
+print(device_details)
+
+# for device_detail in device_details:
+#     if device_detail['id'] == device_details[0]['id']:
+#         print(f"Client IP: {device_detail['ip']}")
+#         print(f"MAC: {device_detail['id']}")
+#         print(f"Health Score: {device_detail['healthScore']}")
+#         print(" ")
+#     elif device_detail['id'] == device_details[1]['id']:
+#         print(f"Connected to {device_detail['deviceType']}")
+#         print(f"WAP IP: {device_detail['ip']}")
--- fmc.py
+++ fmc.py
@@ -8,7 +8,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 url = "https://fmcrestapisandbox.cisco.com/api/fmc_platform/v1/auth/generatetoken"
 headers = {'Content-Type': 'application/json'}
 user = 'taimoorahm'
-pw = 'EBTKgKXJ'
+pw = 'RvXbTr8m'
 
 
 # not using the .json() in the post method because it will return an empty response of 204
@@ -31,4 +31,18 @@ url2 = 'https://fmcrestapisandbox.cisco.com/api/fmc_config/v1/domain/e276abec-e0
 apps_response = requests.get(url2, headers=headers, verify=False).json()
 # print(apps_response)
 
-print(json.dumps(apps_response, indent=2, sort_keys=True))
+# print(json.dumps(apps_response, indent=2, sort_keys=True))
+
+apps_name = apps_response['items']
+# print(apps_name)
+print(json.dumps(apps_name, indent=2, sort_keys=True))
+
+initial = 0
+maxiumum = 25
+
+while initial < maxiumum:
+    app_id = apps_response['items'][initial]['id']
+    application_name = apps_response['items'][initial]['name']
+    print(app_id)
+    print(application_name)
+    initial += 1
